8972fcba26165c1b9b7bf81b71c2011e697acbe2
阿昀 <yunthomas006@gmail.com>
2026-08-22 17:48:43 +0800

    feat(manual): register specialist material overlay

--- src/client/java/dev/totem/nexus/client/TotemNexusClient.java
+++ src/client/java/dev/totem/nexus/client/TotemNexusClient.java
@@ -1,6 +1,7 @@
 package dev.totem.nexus.client;
 
 import dev.totem.nexus.client.manual.NexusManualPageOverlay;
+import dev.totem.nexus.client.manual.NexusSpecialistMaterialPageOverlay;
 import net.fabricmc.api.ClientModInitializer;
 
 /** Client entrypoint for the Nexus-owned Space Unit and death-node interfaces. */
@@ -8,6 +9,7 @@ public final class TotemNexusClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         NexusManualPageOverlay.register();
+        NexusSpecialistMaterialPageOverlay.register();
         NexusClientBootstrap.registerNetworking();
         NexusDeathNodeAdminClientInitializer.registerReceiver();
         NexusMaterialCatalogClientState.register();
